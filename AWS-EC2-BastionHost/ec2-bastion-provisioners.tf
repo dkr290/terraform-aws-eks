a41fae51571dc25f3dfa1cda922797e04e0cfe96
dansadmin@homail.com <dansadmin@homail.com>
2022-09-15 20:15:39 +0200

    adding the right key

--- AWS-EC2-BastionHost/ec2-bastion-provisioners.tf
+++ AWS-EC2-BastionHost/ec2-bastion-provisioners.tf
@@ -5,20 +5,20 @@ resource "null_resource" "copy_ec2_keys" {
   connection {
     type = "ssh"
     host = aws_eip.bastion_eip.public_ip
-    user = "ec2_user"
+    user = "ec2-user"
     password = ""
-    private_key = file("private-key/eks-terraform-key.pem")
+    private_key = file("private-key/eks-key.pem")
   }
 
  provisioner "file" {
-    source = "private-key/eks-terraform-key.pem"
-    destination = "/tmp/eks-terraform-key.pem"
+    source = "private-key/eks-key.pem"
+    destination = "/tmp/eks-key.pem"
    
  }
 
  provisioner "remote-exec" {
     inline = [
-      "sudo chmod 400 /tmp/eks-terraform-key.pem"
+      "sudo chmod 400 /tmp/eks-key.pem"
     ]
    
  }
