10df0aa71cbfbc7c833d7331dcecc78d35845085
dani <d_krasimirov@yahoo.com>
2022-09-25 22:15:05 +0200

    adding csi driver creation

---
+++ AWS-EBS-csi-install/ebs-terraform-manifests/providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1.2"
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.19"
+      #alias   = "aws-eu-central"
+    }
+  }
+
+backend "s3" {
+  bucket = "terraform-infrastate80"
+  key = "dev/ebs-storage/terraform.tfstate"
+  region = "eu-central-1"
+  
+}
+
+
+}
+
+provider "aws" {
+  # Configuration options
+  region  = var.aws_region
+  profile = "default"
+
+
+}
+
+
+
